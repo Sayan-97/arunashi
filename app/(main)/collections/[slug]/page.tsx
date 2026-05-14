4445678c098f2962b5d85418c672d66187277c78
Sayan-97 <sdey4232@gmail.com>
2026-05-15 01:00:05 +0530

    feat: add collection products page

---
+++ app/(main)/collections/[slug]/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import ProductCard from "@/components/shared/product-card";
+import { products } from "@/constants";
+import HeroImg from "@/public/collection-hero-bg.png";
+
+export default function CollectionDetailPage() {
+  return (
+    <main className="pb-15 space-y-25">
+      <section>
+        <Image
+          src={HeroImg}
+          alt="Hero Image"
+          priority
+          placeholder="blur"
+          className="w-full h-103 object-cover"
+        />
+      </section>
+      <section className="app_container grid md:grid-cols-2 lg:grid-cols-3 gap-x-11.75 gap-y-15">
+        {products.slice(0, 6).map((product) => (
+          <ProductCard
+            key={product.id}
+            image={product.image}
+            name={product.name}
+            link={`/products/${product.id}`}
+          />
+        ))}
+      </section>
+    </main>
+  );
+}
