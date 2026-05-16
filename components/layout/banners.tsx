011d6dff5f6946c47e8376682e6f3a58a6fe3a40
Sayan-97 <sdey4232@gmail.com>
2026-05-16 12:05:01 +0530

    chore: removed carousel from banner

--- components/layout/banners.tsx
+++ components/layout/banners.tsx
@@ -1,44 +1,22 @@
-"use client";
-
-import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
 import { banners } from "@/constants";
 
 export default function Banners() {
+  const banner = banners[0];
+
   return (
     <section>
-      <Carousel
-        opts={{ loop: true }}
-        plugins={[
-          Autoplay({
-            delay: 4500,
-            stopOnInteraction: false,
-          }),
-        ]}
-      >
-        <CarouselContent>
-          {banners.map((banner) => (
-            <CarouselItem key={banner.id}>
-              <div className="relative w-full h-[381px]">
-                <Image
-                  src={banner.image}
-                  alt={`Banner Image`}
-                  fill
-                  priority
-                  placeholder="blur"
-                  sizes="100vw"
-                  className="object-cover object-top-left -z-10"
-                />
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
+      <div className="relative w-full h-[381px]">
+        <Image
+          src={banner.image}
+          alt="Banner Image"
+          fill
+          priority
+          placeholder="blur"
+          sizes="100vw"
+          className="object-cover object-top-left -z-10"
+        />
+      </div>
     </section>
   );
 }
